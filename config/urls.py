1a8ff72e104c14a4f6d4c0d009101e003accffae
Rumman Shahzad <76628969+rshaz2713@users.noreply.github.com>
2026-03-04 15:13:16 -0500

    Fix basic URL routing

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("core.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", include("dashboard.urls")),
 ]
