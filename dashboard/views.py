7e6992b2a34758fd69f52c0c66314a968736e1d5
Rumman Shahzad <76628969+rshaz2713@users.noreply.github.com>
2026-03-04 15:13:31 -0500

    Render views for login and the homepage

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-
-# Create your views here.
 
 @login_required
 def dashboard_home(request):
-    return HttpResponse(f"Dashboard running. Logged in as {request.user.username}.")
+    return render(request, "dashboard/home.html")
+    
