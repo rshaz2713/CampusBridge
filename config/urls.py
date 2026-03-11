774321cd1187eb5689c684021c028c362b5eca0c
Rumman Shahzad <76628969+rshaz2713@users.noreply.github.com>
2026-03-10 20:04:29 -0400

    Authentication and Bootstrap UI (#33)
    * Create requirements.txt
    
    * Create Django project
    
    * Update README.md
    
    * Create core and dashboard apps with basic URL routing
    
    * Create template files
    
    * Fix template base directory
    
    * Fix basic URL routing
    
    * Render views for login and the homepage
    
    * Update README.md
    
    * Add some comments to code files
    
    * Update README.md with steps on how to add a new page
    
    * Update home.html
    
    * Improve authentication UI and navbar logic
    
    * Fix static files 404 error
    
    * Improve register page and auth UI
    
    ---------
    
    Co-authored-by: Raghubir Bhandari <bhandariraghubir23@gmail.com>

--- config/urls.py
+++ config/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from dashboard.views import dashboard_home
+
 
 # Root URL configuration. Routes traffic to appropriate Django apps.
 # You can check specific URLs of apps core and dashboard.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("core.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("dashboard/", include("dashboard.urls")),
+    path("accounts/login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("dashboard/", dashboard_home, name="dashboard_home"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", views.home, name="home"),
+    path("register/", views.register_view, name="register"),
 ]
