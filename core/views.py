8725775bed172ae48dbe62dde8b1e65f26632356
Rumman Shahzad <76628969+rshaz2713@users.noreply.github.com>
2026-04-02 14:23:47 -0400

    BUG: Fix missing decorator imports

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from django import forms
 from .models import Profile
 
