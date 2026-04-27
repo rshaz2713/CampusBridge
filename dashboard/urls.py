2d870c7de5c169a78cacdfec5d319232f1552566
Rumman Shahzad <76628969+rshaz2713@users.noreply.github.com>
2026-04-27 12:29:09 -0400

    Fix resource unavailable URL

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (dashboard_home, degree_audit_view, professor_search_view, create_announcement_view, 
                     announcement_list_view, announcement_detail_view,course_management_view, 
-                    course_detail_view,update_grade_view,available_courses_view,enroll_course_view, withdraw_registered_course)
+                    course_detail_view,update_grade_view,available_courses_view,enroll_course_view, withdraw_registered_course,
+                    resource_unavailable)
 
 urlpatterns = [
     path("", dashboard_home, name="dashboard_home"),
@@ -19,4 +20,6 @@ urlpatterns = [
     path("available-courses/", available_courses_view, name="available_courses"),
     path("enroll-course/<int:course_id>/", enroll_course_view, name="enroll_course"),
     path("withdraw-course/<int:course_id>/", withdraw_registered_course, name="withdraw_course"),
+
+    path("resource-unavailable/<str:resource_id>/", resource_unavailable, name="resource_unavailable"),
 ]
