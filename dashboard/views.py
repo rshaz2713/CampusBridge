a480961989bd1125dd874985d2e14ee0bb5a6e09
Rumman Shahzad <76628969+rshaz2713@users.noreply.github.com>
2026-04-27 14:46:01 -0400

    BUG: Fix broken imports

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from core.models import Profile
 from core.constants.resources import RESOURCE_INFO
-from models import StudentRecord, Announcement, AnnouncementRead, Course, Enrollment
+from .models import StudentRecord, Announcement, AnnouncementRead, Course, Enrollment
 from django.shortcuts import get_object_or_404
-from forms import GradeUpdateForm, AnnouncementForm
+from .forms import GradeUpdateForm, AnnouncementForm
 
 @login_required
 def dashboard_home(request):
